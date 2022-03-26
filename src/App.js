181c0cb6493a529da2c6b8fd1532e70111fc21db
Scott Green <sagreenxyz@gmail.com>
2022-03-26 12:05:23 -0500

    2. stub out DisplayIdeas component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+function DisplayIdeas(props) {
+  return (
+    <div>
+      DisplayIdeas
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      Hello World!
+      <DisplayIdeas />
     </div>
   );
 }
