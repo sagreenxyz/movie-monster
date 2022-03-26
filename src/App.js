43fe24954e1525a9481336fab626372923e5e102
Scott Green <sagreenxyz@gmail.com>
2022-03-26 12:12:58 -0500

    7. stub IdeasForm component

--- src/App.js
+++ src/App.js
@@ -11,6 +11,14 @@ function DisplayIdeas(props) {
   )
 }
 
+function IdeasForm(props) {
+  return (
+    <div>
+      IdeasForm
+    </div>
+  )
+}
+
 function App() {
   const [ideas, setIdeas] = useState([
     "Movie about a guy who gets bit by a radioactive mole rat",
@@ -26,6 +34,7 @@ function App() {
     <main>
       <h1>Welcome, Super Awesome Movie Monster Think Tank!</h1>
       <DisplayIdeas ideas={ideas} />
+      <IdeasForm />
     </main>
   )
 }
