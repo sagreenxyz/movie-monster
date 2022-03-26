7250be77123f4ab38d6b3017bb429fb192092542
Scott Green <sagreenxyz@gmail.com>
2022-03-26 09:43:47 -0500

    1. creating stubs for DisplayIdeas and Form components; Form component may be renamed later

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
+function DisplayIdeas(props) {
+  return (
+    <div>
+      DisplayIdeas Component
+    </div>
+  )
+}
+
+function Form() {
+  return (
+    <div>
+      Form Component
+    </div>
+  )
+}
+
 function App() {
+  const [ideas, setIdeas] = useState([
+    "Movie about a guy who gets bit by a radioactive mole rat",
+    "Romcom about two developers over Zoom",
+    "Horror film where AWS goes down for 24 hours"
+  ]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <h1>Welcome, Super Awesome Movie Monster Think Tank!</h1>
+
+        <br />
+
+        <h1>Submit your ideas down below:</h1>
+
+        <DisplayIdeas />
+        <Form />
+
+      </main>
     </div>
   );
 }
