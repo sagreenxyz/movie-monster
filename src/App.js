deb339fb917a0b8bbc35955e976ecd435108af85
Scott Green <sagreenxyz@gmail.com>
2022-03-26 12:18:54 -0500

    9. form and handleSubmit function in IdeasForm component

--- src/App.js
+++ src/App.js
@@ -12,9 +12,19 @@ function DisplayIdeas(props) {
 }
 
 function IdeasForm(props) {
+  let [input, setInput] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    props.addIdeas(input)
+  }
+
   return (
     <div>
-      IdeasForm
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={(e) => setInput(e.target.value)} />
+        <button type="submit">Submit your Idea!</button>
+      </form>
     </div>
   )
 }
